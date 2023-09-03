2ce7049e2ce6c1fe194dbc5c5fab5e63f98b1510
Casey Scarborough <casey.scarborough@nscorp.com>
2023-09-02 23:32:13 -0400

    Print configuration at startup

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from plugin.db_utils import attempt_init_db
-from plugin.consts import BIND_ADDRESS, BIND_PORT
+from plugin.consts import (
+    BIND_ADDRESS,
+    BIND_PORT,
+    CONFIG_DIRECTORY,
+    DATA_DIRECTORY,
+    EXTERNAL_URL
+)
 from plugin.server import LocalAudioHandler
 import http
 import threading
 
 if __name__ == "__main__":
-    print("Initializing database...")
+    print("\nUsing configuration:")
+    print(f"- BIND_ADDRESS: {BIND_ADDRESS}")
+    print(f"- BIND_PORT: {BIND_PORT}")
+    print(f"- EXTERNAL_URL: {EXTERNAL_URL}")
+    print(f"- DATA_DIRECTORY: {DATA_DIRECTORY}")
+    print(f"- CONFIG_DIRECTORY: {CONFIG_DIRECTORY}")
+    print("\nInitializing database...")
     attempt_init_db()
-    print("Starting server...")
+    print(f"Database is initialized, server will now start...")
     server = http.server.ThreadingHTTPServer((BIND_ADDRESS, BIND_PORT), LocalAudioHandler)
     server.serve_forever()
